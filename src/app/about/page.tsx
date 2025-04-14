c84fd08003c849aa62013f96784065e9cee7a5d9
Marcus Falck <marcus.falck@awardit.com>
2025-04-14 10:59:39 +0200

    chore: push WIP

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import { ThemeToggle } from "@/components/theme-toggle";
+
+export default function About() {
+  return (
+    <div>
+      <ThemeToggle />
+      <h1>About</h1>
+    </div>
+  );
+}
